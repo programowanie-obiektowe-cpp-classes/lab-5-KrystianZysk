8235d32f4c8b920e70110962ee70b62ba5575304
KrystianZysk <zkrystian.z08@gmail.com>
2024-11-25 01:23:28 +0100

    Solution

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,17 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector<char> v;
+    v.reserve(people.size());
+    for(auto& people:people)
+        people.birthday();
+
+    for(auto& people:people)
+        if (people.isMonster()){
+            v.push_back('n');
+        } else{
+            v.push_back('y');
+        }
+    std::reverse(v.begin(), v.end());
+    return v;
 }
